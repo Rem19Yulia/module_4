e156f14094e5b18b1515938a2d837572f75f6af2
Rem19Yulia <kyv190591@gmail.com>
2024-08-05 23:56:22 +0300

    Update module_4_2.py

--- module_4_2.py
+++ module_4_2.py
@@ -1,9 +1,17 @@
 def test_function():
     #inner_function()- ошибка
-    def inner_function(x):
-        print("Я в области видимости функции test_function")
+    def inner_function():
+        return("Я в области видимости функции test_function")
+
+# Вызов вложенной функции внутри test_function и вывод результата
+    result = inner_function()
+    print(result)
 
     inner_function()
 
+# Попытка вызвать inner_function вне функции test_function
+try:
+    result = inner_function()
+    print(result)
 
-    #inner_function() #ошибка, так как inner_fuction не видна за пределами test_function
+ #ошибка, так как inner_fuction не видна за пределами test_function
